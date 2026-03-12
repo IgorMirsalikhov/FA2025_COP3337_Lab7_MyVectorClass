1dbc514c13d881d1b522d760a89d1c28fdcce0a4
Igor Mirsalikhov <imirsalikhov@floridapoly.edu>
2026-03-11 20:59:04 -0400

    Update MyVector.cpp

--- MyVector.cpp
+++ MyVector.cpp
@@ -1,73 +1,85 @@
 #include "MyVector.hpp"
 #include <iostream>
 
-// Constructor
-MyVector::MyVector(int capacity) : size(0) {
+MyVector::MyVector(int capacity) {
     this->capacity = capacity;
+    size = 0;
     elements = new int[capacity];
-}
-
-// Destructor
-MyVector::~MyVector() {
-    delete [] elements;
 }
 
 // Copy constructor
 MyVector::MyVector(const MyVector& other) {
-    size = other.size;
     capacity = other.capacity;
+    size = other.size;
     elements = new int[capacity];
-
-    //copy elements
     for(int i = 0; i < size; i++) {
         elements[i] = other.elements[i];
     }
 }
 
-void MyVector::push_back(int value) {
-    if (size >= capacity) {
-        allocate_memory(capacity * 2);
-    }
-    elements[size] = value;
-    size++;
+MyVector::~MyVector() {
+    delete [] elements;
 }
-
-int MyVector::pop_back(void) {
-    if (size > 0) {
-        if (size - 1 < capacity / 2) {
-            allocate_memory(capacity / 2);
-        }
-        return elements[--size];
-    }
-    else {
-        // Throw an exception
-        throw "The vector is empty!";
-    }
-}
-
 
 void MyVector::print() const {
     std::cout << "[ ";
     for (int i = 0; i < size; i++) {
         std::cout << elements[i] << ' ';
     }
-    std::cout << "]\n"; 
+    std::cout << "]\n";
 }
 
-
-void MyVector::allocate_memory(int memory_size) {
-    capacity = memory_size;
-    int *old = elements;
-    // Allocate a new memory (bigger or smaller)
-    elements = new int[memory_size];
-    for (int i = 0; i < size; i++) {
-        elements[i] = old[i];
+void MyVector::push_back(int val) {
+    if (full()) {
+        allocateMemory(capacity * 2);
     }
-    // Deallocate the old memory
-    delete [] old;
+    elements[size] = val;
+    size++;
 }
 
+bool MyVector::empty() const {
+    return size == 0;
+}
 
-int& MyVector::at(int index) {
+bool MyVector::full() const {
+    return size == capacity;
+}
 
+void MyVector::allocateMemory(int memory_size) {
+    capacity = memory_size;
+    // Save the old address of the array
+    int* old_array = elements;
+    // Allocate new memory
+    elements = new int[capacity];
+
+    // Copy the elements
+    for (int i = 0; i < size; i++) {
+        elements[i] = old_array[i];
+    }
+    delete [] old_array;
+}
+
+int MyVector::pop_back() {
+    if (!empty()) {
+        if (size < capacity / 2) {
+            allocateMemory(capacity / 2);
+        }
+        return elements[--size];
+    }
+    else {
+        throw "Empty vector";
+    }
+}
+
+int MyVector::getCapacity() const {
+    return capacity;
+}
+
+int& MyVector::at(int index) const {
+    if (index < 0 || index >= size) {
+        throw "At: out of boundaries\n";
+    }
+    else {
+        return elements[index];
+    }
 }
